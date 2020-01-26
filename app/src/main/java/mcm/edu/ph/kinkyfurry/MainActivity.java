e9d21b1510de65a23cbdeab3b4902e1e7634ffa5
Kiboot <kiboot@users.noreply.github.com>
2020-01-26 20:40:21 +0800

    created payment option activity

--- app/src/main/java/mcm/edu/ph/kinkyfurry/MainActivity.java
+++ app/src/main/java/mcm/edu/ph/kinkyfurry/MainActivity.java
@@ -2,15 +2,45 @@ package mcm.edu.ph.kinkyfurry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TableRow item1,item2,item3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        item1 = findViewById(R.id.item1);
+        item2 = findViewById(R.id.item2);
+        item3 = findViewById(R.id.item3);
 
+        item1.setOnClickListener(this);
+        item2.setOnClickListener(this);
+        item3.setOnClickListener(this);
     }
+    @Override
+    public void onClick(View arg0) {
+        switch (arg0.getId()) {
+            case R.id.item1:
+
+                break;
+            case R.id.item2:
+
+                break;
+            case R.id.item3:
+
+                break;
+
+        }}
+
+
+
 }
---
+++ app/src/main/java/mcm/edu/ph/kinkyfurry/PaymentOption.java
@@ -0,0 +1,24 @@
+package mcm.edu.ph.kinkyfurry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class PaymentOption extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_option);
+    }
+    @Override
+    public void onClick(View arg0) {
+        switch (arg0.getId()) {
+
+
+
+
+        }}
+
+}
